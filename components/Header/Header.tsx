70fc653cfd035ca7f07c37215e237ecefabd0a56
Abdullah Arif <160360851+AbdullahArif17@users.noreply.github.com>
2024-11-06 22:22:38 +0500

    figma design

---
+++ components/Header/Header.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+  return (
+    <header className="w-full max-w-[1920px] bg-[#A29875] h-[134px] flex items-center">
+      <h1 className="text-4xl font-bold w-[439px] h-[94px] p-8 text-[75px] font-normal font-mono italic ml-[36px]">MANZZARI</h1>
+    </header>
+  );
+};
+
+export default Header;
+
+
+
